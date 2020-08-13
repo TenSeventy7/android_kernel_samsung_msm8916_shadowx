0b0c2583bccde95401a0bc2cdf25cec804054b41
John Vincent <corcegajovic@gmail.com>
2020-08-13 12:28:39 +0800

    build/anykernel: Change to 'grandmax3g'

--- anykernel/ark.sh
+++ anykernel/ark.sh
@@ -24,7 +24,7 @@
 # Galahad Primary setup
 # begin properties
 properties() { '
-kernel.string=ArKernel for Fortuna - Project ShadowX Core
+kernel.string=ArKernel for GrandMax - Project ShadowX Core
 do.devicecheck=1
 do.modules=1
 do.systemless=0
